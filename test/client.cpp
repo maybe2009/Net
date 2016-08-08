7d246d0e0b62505ae9e0cd0c988a80151f74e9e1
maybe2009 <ox07c00@163.com>
2016-08-08 19:37:11 +0800

    add test

---
+++ test/client.cpp
@@ -0,0 +1,27 @@
+//
+// Created by DW on 2016/8/8.
+//
+#include "NetSocket.h"
+
+#include <fcntl.h>
+
+#include <iostream>
+#include <string>
+
+int main() {
+  try {
+    SocketAddressV4 address(1234, std::string("127.0.0.1"));
+    NetSocket socket0(AF_INET, SOCK_STREAM, 0);
+
+    if (socket0.Connect((const ADDR*)address.Get(), address.Size()) == 0) {
+      std::cout << "Connect Success "  << endl;
+    } else {
+      std::cout << "Connect Error " << endl;
+    }
+  }
+  catch (NetSocketException& e) {
+    std::cout << e.what() << endl;
+  }
+
+  return 0;
+}
