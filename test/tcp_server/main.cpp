eb08acc24deb05fae0d79460dc898749203959fa
maybe2009 <ox07c00@163.com>
2016-08-09 10:03:52 +0800

    bug fix

--- test/tcp_server/main.cpp
+++ test/tcp_server/main.cpp
@@ -1,24 +1,29 @@
 //
 // Created by DW on 2016/8/8.
 //
-
 #include "NetSocket.h"
 
+#include <unistd.h>
 #include <iostream>
 
 int main() {
   try {
     SocketAddressV4 address(1234, std::string("127.0.0.1"));
     NetSocket socket0(AF_INET, SOCK_STREAM, 0);
+    socket0.Bind((const ADDR*)address.Get(), address.Size());
+    std::cout << "Listening..." << std::endl;
+    socket0.Listen(5);
 
-    if (socket0.Connect((const ADDR*)address.Get(), address.Size()) == 0) {
-      std::cout << "Connect Success "  << std::endl;
+    if ( FD client_fd = socket0.Accept(NULL, NULL)) {
+      std::cout << "New fd " << client_fd << std::endl;
+      close(client_fd);
     } else {
-      std::cout << "Connect Error " << std::endl;
+      std::cout << "Accept Error " << client_fd << std::endl;
     }
   }
   catch (NetSocketException& e) {
     std::cout << e.what() << std::endl;
   }
+
   return 0;
 }
